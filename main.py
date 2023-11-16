a877d0988408b3b1d2e0ddc3d7976bd362ddf15e
Erisae <xiayuhan18@mails.ucas.ac.cn>
2023-11-16 12:09:57 -0500

    transfer to sns

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI, Response, HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -24,7 +26,23 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # ses
-ses_client = boto3.client('ses', region_name='us-east-2')
+sns_client = boto3.client('sns', region_name='us-east-2')
+# sns
+topic_arn = ''  # replace
+
+
+def publish_sns_message(action, data):
+    message = json.dumps({'action': action, 'data': data})
+    sns_client.publish(
+        TopicArn=topic_arn,
+        Message=message,
+        MessageAttributes={
+            'Action': {
+                'DataType': 'String',
+                'StringValue': action
+            }
+        }
+    )
 
 
 # This function returns a DataService instance.
@@ -73,43 +91,60 @@ async def get_adoption_by_id(adoptionId: str):
 async def create_adoption(adoption: AdoptionCreate, adopter_email: str = None, shelter_email: str = None, pet_name: str = None):
     """Create a new adoption request."""
     new_adoption = adoptions_resource.create_adoption(adoption.model_dump())
-    if adopter_email and shelter_email:
-        send_email(
-            [adopter_email, shelter_email],
-            "Adoption Request Notice",
-            f"<h1>Adoption Request</h1><p>{adopter_email} has requested to adopt {pet_name} from {shelter_email}</p>"
-                   )
+    try:
+        adoption_data = {
+            'adoption_id': new_adoption.adoptionId,
+            'status': new_adoption.status,
+            'adopter_email': adopter_email,
+            'shelter_email': shelter_email,
+            'pet_name': pet_name
+        }
+        publish_sns_message('CREATE', adoption_data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
     return new_adoption
 
 
 @app.put("/adoptions/{adoptionId}", response_model=AdoptionRspModel)
-async def update_adoption_status(adoptionId: str, updated_data: AdoptionUpdate, adopter_email: str = None, shelter_email: str = None, pet_name: str = None):
+async def update_adoption_status(adoption_id: str, updated_data: AdoptionUpdate, adopter_email: str = None, shelter_email: str = None, pet_name: str = None):
     """Shelter updates adoption status."""
-    updated_adoption = adoptions_resource.update_adoption_status(adoptionId, updated_data)
+    updated_adoption = adoptions_resource.update_adoption_status(adoption_id, updated_data)
     if not updated_adoption:
         raise HTTPException(status_code=404, detail="Adoption not found")
-    if adopter_email and shelter_email:
-        send_email(
-            [adopter_email, shelter_email],
-            "Adoption Status Change Notice",
-            f"<h1>Adoption Status Changed</h1><p>{adopter_email}'s adoption request to {pet_name} from {shelter_email} has been updated to {updated_data.status}</p>"
-        )
+
+    try:
+        adoption_data = {
+            'adoption_id': adoption_id,
+            'status': updated_data.status,
+            'adopter_email': adopter_email,
+            'shelter_email': shelter_email,
+            'pet_name': pet_name
+        }
+        publish_sns_message('UPDATE', adoption_data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
     return updated_adoption
 
 
 @app.delete("/adoptions/{adoptionId}")
-async def delete_adoption(adoptionId: str, adopter_email: str = None, shelter_email: str = None, pet_name: str = None):
+async def delete_adoption(adoption_id: str, adopter_email: str = None, shelter_email: str = None, pet_name: str = None):
     """Delete an adoption request."""
-    success = adoptions_resource.delete_adoption(adoptionId)
+    success = adoptions_resource.delete_adoption(adoption_id)
     if not success:
         raise HTTPException(status_code=404, detail="Adoption not found")
-    if adopter_email and shelter_email:
-        send_email(
-            [adopter_email, shelter_email],
-            "Adoption Deleted",
-            f"<h1>Adoption Deleted</h1><p>{adopter_email}'s adoption request to {pet_name} from {shelter_email} has been deleted</p>"
-        )
+    try:
+        delete_data = {
+            'adoption_id': adoption_id,
+            'adopter_email': adopter_email,
+            'shelter_email': shelter_email,
+            'pet_name': pet_name
+        }
+        publish_sns_message('DELETE', delete_data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
     return {"status": "Adoption deleted successfully"}
 
 
